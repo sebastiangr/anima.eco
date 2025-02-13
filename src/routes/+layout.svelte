938f7c32528dafbe2ea2e23103c3474403b697aa
sebastiangr <sebastiangr@proton.me>
2025-02-13 12:39:56 -0500

    Add Vercel Analytics integration and update dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import '../app.postcss';
+  import { dev } from '$app/environment';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="body-bg flex items-center justify-center min-h-screen">
